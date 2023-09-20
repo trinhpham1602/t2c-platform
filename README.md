1b1632721e5d118810cc73915480e716bd6270b6
pdtrinh <phamdangtrinh1998@gmail.com>
2023-09-20 22:56:50 +0700

    add swagger

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # t2c-platform
+
+Open Swagger:
+    http://localhost:5100/t2c/swagger-ui/index.html
--- src/main/java/com/t2c/base/controllers/TestController.java
+++ src/main/java/com/t2c/base/controllers/TestController.java
@@ -1,18 +1,14 @@
 package com.t2c.base.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-
   
 @RestController
 public class TestController {
   
     @GetMapping("/")
-    public String helloGFG(@AuthenticationPrincipal Jwt jwt)
+    public String helloGFG(String name)
     {
-        return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
+        return String.format("Hello, %s!", name);
     }
 }
