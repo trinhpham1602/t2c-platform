cc936ca1a07f3aa7f8755e94dccbb1b1738bda05
pdtrinh <phamdangtrinh1998@gmail.com>
2024-02-29 21:53:49 +0700

    add spring security

---
+++ src/main/java/t2c/base/core/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package t2c.base.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
